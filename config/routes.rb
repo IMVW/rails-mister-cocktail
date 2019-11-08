b771fdd17b45dc7efcbb1499757170176fa81fbc
Iris Vermeulen <iris.m.vermeulen@gmail.com>
2019-11-08 11:39:30 +0100

    inbetween save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/:id', to: 'cocktails#show'
+  # get 'cocktails/new'
+  # post 'cocktails', to: 'cocktails#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
+
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'JSON'
+
+Ingredient.delete_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+json = open(url).read
+ingredients = JSON.parse(json)
+ingredients = ingredients['drinks']
+ingredients.each do |ingredient|
+  object = {
+    name: ingredient['strIngredient1']
+  }
+  Ingredient.create!(object)
+end
