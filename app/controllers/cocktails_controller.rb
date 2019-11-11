32ea60194e17bc704df02de1db40aa2a57be5cbc
Iris Vermeulen <iris.m.vermeulen@gmail.com>
2019-11-11 14:30:07 +0100

    late save

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,6 +6,8 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+
   end
 
   def new
@@ -24,6 +26,6 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require("cocktail").permit(:name)
+    params.require(:cocktail).permit(:name)
   end
 end
